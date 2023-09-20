81ccc188b32c36fd0b5eb13b90483b0320ba2257
icktae <taechil77@naver.com>
2023-09-20 18:12:45 +0900

    render_all def

--- character_move.py
+++ character_move.py
@@ -7,6 +7,13 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+def render_all(x, y):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        delay(0.1)
+
+
 def run_circle() :
     print('CIRCLE')
 
@@ -14,35 +21,30 @@ def run_circle() :
     cy = 300
     r = 200
 
-    for deg in range(0, 360, 5) :
+    for deg in range(0, 360, 4) :
         x = cx + r * math.cos(deg / 360 * 2 * math.pi)
         y = cy + r * math.sin(deg / 360 * 2 * math.pi)
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,y)
-        delay(0.01)
+        render_all(x, y)
+        
 
+        
 def run_square() :
     print('SQUARE')
 
     # bottom
     for x in range(50, 750, 10) :
         clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,90)
-        delay(0.01)
+        render_all(x,90)
 
     # up
     for y in range(90, 550, 10) :
         clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(750,y)
-        delay(0.01)
+        render_all(750,y)
 
         
     pass
 
 while True :
-   # run_circle()
+    run_circle()
     run_square()
     break
