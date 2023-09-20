a057a376cfd998839af3879488492978f4dfeaa0
icktae <taechil77@naver.com>
2023-09-20 17:46:29 +0900

    print suc

--- character_move.py
+++ character_move.py
@@ -9,6 +9,10 @@ character = load_image('character.png')
 
 def run_circle() :
     print('CIRCLE')
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(400,90)
+    delay(1)
     pass
 
 def run_square() :
